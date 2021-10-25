ce8942583c30f5055f9e3b9dfa8a9ab1c708bf82
Leandro Santos <lleandro.msn@gmail.com>
2021-10-25 12:36:40 -0300

    Voiced Commands.
    Added Some new Voiced Commands.

--- dist/game/data/scripts/handlers/MasterHandler.java
+++ dist/game/data/scripts/handlers/MasterHandler.java
@@ -296,12 +296,15 @@ import handlers.voicedcommandhandlers.Banking;
 import handlers.voicedcommandhandlers.CcpVCmd;
 import handlers.voicedcommandhandlers.ChangePassword;
 import handlers.voicedcommandhandlers.ChatAdmin;
+import handlers.voicedcommandhandlers.CombineTalismans;
 import handlers.voicedcommandhandlers.Debug;
 import handlers.voicedcommandhandlers.EvenlyDistributeItems;
 import handlers.voicedcommandhandlers.Hellbound;
 import handlers.voicedcommandhandlers.ItemBufferVCmd;
 import handlers.voicedcommandhandlers.Lang;
+import handlers.voicedcommandhandlers.MakeAncientAdena;
 import handlers.voicedcommandhandlers.OnlineVCmd;
+import handlers.voicedcommandhandlers.OpenAtod;
 import handlers.voicedcommandhandlers.PingVCmd;
 import handlers.voicedcommandhandlers.PremiumVCmd;
 import handlers.voicedcommandhandlers.PvpZoneVCmd;
@@ -659,6 +662,9 @@ public class MasterHandler
 		(CustomServerConfigs.ALLOW_REPAIR_COMMAND ? RepairVCmd.class : null),
 		(CustomServerConfigs.ALLOW_TELEPORTS_COMMAND ? TeleportsVCmd.class : null),
 		PingVCmd.class,
+		CombineTalismans.class,
+		MakeAncientAdena.class,
+		OpenAtod.class,
 		(Config.L2JMOD_ALLOW_WEDDING ? Wedding.class : null),
 		(GetRewardVoteSystemConfigs.ENABLE_VOTE_SYSTEM ? RewardVote.class : null),
 	};
